0231ba80d565b7fd9357d12a92dfaa0b77050b5a
vitorsg27 <vitors92@hotmail.com>
2024-07-11 09:54:38 -0300

    Added image resize with ImageMagick for menu

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,4 +1,12 @@
 class Produto < ApplicationRecord
     belongs_to :category
     has_one_attached :image_file
+
+    def thumbnail
+        image_file.variant(resize_to_limit: [300, 300]).processed
+      end
+    
+      def small
+        image_file.variant(resize_to_limit: [100, 100]).processed
+      end
 end
